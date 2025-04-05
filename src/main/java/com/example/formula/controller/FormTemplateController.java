4c8b9db6055d561c39cd9278ff7738dc9e4d3eb3
Codewizard-18 <jaswanthm811@gmail.com>
2025-04-05 09:59:05 +0530

    Form template entities and apis creation,testing

---
+++ src/main/java/com/example/formula/controller/FormTemplateController.java
@@ -0,0 +1,52 @@
+package com.example.formula.controller;
+
+import com.example.formula.entity.FormTemplate;
+import com.example.formula.services.FormTemplateService;
+import com.example.formula.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/public")
+public class FormTemplateController {
+
+    @Autowired
+    private FormTemplateService formTemplateService;
+
+    @GetMapping("/forms/{ownerId}")
+    public ResponseEntity<List<FormTemplate>> getall(@PathVariable String ownerId){
+        try {
+            List<FormTemplate> forms = formTemplateService.getAllForms(ownerId);
+            return new ResponseEntity<>(forms, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/forms/{ownerId}/{id}")
+    public ResponseEntity<FormTemplate> getone(@PathVariable String ownerId,@PathVariable String id){
+        try {
+            FormTemplate formTemplate = formTemplateService.getForm(id);
+            return new ResponseEntity<>(formTemplate,HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/forms")
+    public ResponseEntity<FormTemplate> postone(@RequestBody FormTemplate formTemplate){
+        try {
+            FormTemplate form = formTemplateService.postForm(formTemplate);
+            return new ResponseEntity<>(form,HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
--- src/main/java/com/example/formula/repository/FormTemplateRepository.java
+++ src/main/java/com/example/formula/repository/FormTemplateRepository.java
@@ -4,6 +4,9 @@ import com.example.formula.entity.FormTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FormTemplateRepository extends MongoRepository<FormTemplate,String> {
+    List<FormTemplate> findAllByOwnerId(String ownerId);
 }
