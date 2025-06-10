dcc852c395d71462ebbdd88c97662553afaca492
Codewizard-18 <jaswanthm811@gmail.com>
2025-06-10 18:45:29 +0530

    bugs

--- src/main/java/com/example/formula/FormulaApplication.java
+++ src/main/java/com/example/formula/FormulaApplication.java
@@ -9,8 +9,8 @@ import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class FormulaApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+//		Dotenv dotenv = Dotenv.configure().load();
+//		System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
 		SpringApplication.run(FormulaApplication.class, args);
 	}
 }
